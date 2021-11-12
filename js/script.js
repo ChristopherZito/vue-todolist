4c31af113f78e57877d9afbf38294cf5352d4d78
christopherzito <christopherzito11@gmail.com>
2021-11-12 12:23:59 +0100

    Upload file base

---
+++ js/script.js
@@ -0,0 +1,15 @@
+var list = new Vue (
+    {
+        el:"#container",
+        data:{
+            object:[
+                "obj 1",
+                "obj 2",
+                "obj 3",
+                "obj 4",
+                "obj 5",
+                "obj 6",
+            ]
+        },
+    }
+);
